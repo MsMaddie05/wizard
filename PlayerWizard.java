eeaf7dc8b2856384a8f692b85fbca7122524daad
MsMaddie05 <a.griesman@ufl.edu>
2025-03-20 02:44:24 -0400

    Create PlayerWizard.java

---
+++ PlayerWizard.java
@@ -0,0 +1,17 @@
+public class PlayerWizard extends Wizard{
+	public PlayerWizard(String n, int h, int m){ //Uses overloaded constructor
+  	super(n,h,m);
+  }
+  
+  public void reverseBoost(Wizard w){ //skips target's turn |Increases your health by 10% on 3rd attack
+  
+  }
+  
+  public void fireSword(Wizard w){ //Increases firesword damage to target by 20% per time hit
+  
+  }
+  
+  public void bigBang(Wizard w){ //deals 20% damage of target's current health | deals 35% every 3 runs
+  
+  }
+}
