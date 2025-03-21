cbef2542c20628da3be6b4333e2d3d22541e6b5b
Paul Coppolino <hta.infinite@gmail.com>
2025-03-21 01:53:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,23 @@
 import java.util.*;
 
-public class Main{
-	
-	private int failsafe;
+public class Main {
 
+	public static void main(String[] args) {
+	int failsafe;
 	String name;
 	Scanner input = new Scanner(System.in); //scanner reads your output
 	System.out.println("Hey, wizard! Choose a name and begin combat! : ");
-  name = input.nextLine(); //ex. "Lola"
-	
-  failsafe = 0;
-  
-  Wizard Player = new Wizard(name,100,40); //obj type, name, new keyword, obj type
+	name = input.nextLine(); //ex. "Lola"
+
+	failsafe = 0;
+
+	Wizard Player = new Wizard(name,100,40);
 	Wizard Enemy = new Wizard(); //default constructor. Name will always be "Logrith".
-  
-  while(Player.getHealth() > 0 && Enemy.getHealth() > 0 && failsafe <= 10){
-  
-  	failsafe++;
-  }
-  
+
+	while(Player.getHealth() > 0 && Enemy.getHealth() > 0 && failsafe <= 10) {
+		Player.turn(Enemy); //player's turn. Passing in enemy as the target.
+		Enemy.turn(Player); //enemy's turn. Passing in player as the target.
+      		failsafe++;
+		}
+	}
 }
